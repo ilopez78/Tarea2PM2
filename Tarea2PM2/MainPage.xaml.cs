37fb9b9a4aaed82fc376c3757e484d74ddd9939e
ilopez78 <99553835+ilopez78@users.noreply.github.com>
2022-06-24 13:27:06 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2PM2/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea2PM2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+
+
+            var datos = new Datos
+            { Nombres = nom.Text, Apellidos = ape.Text, Edad = edad.Text, Email = mail.Text };
+
+            await Navigation.PushAsync(new Page2(datos));
+        }
+    }
+}
